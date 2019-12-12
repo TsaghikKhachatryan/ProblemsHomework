4cb4e5782ca5d3499130512280af5e65e504630b
TsaghikKhachatryan <khachatryan.tsaghik@gmail.com>
2019-12-12 20:39:02 +0400

    prob1 solved

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Problem1 : initializing an array and printing
+        /*int arr[]={1,2,3,4,5,6};
+        for (int i = 0; i <arr.length; i++) {
+            System.out.println(arr[i]);
+        }*/
+
+    }
+}
